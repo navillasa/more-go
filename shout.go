faada8f980fc1c48e6614502ac0422f4abe97167
Natalie Ventura Villasana <nat@storj.io>
2018-03-19 16:48:09 -0400

    state and behavior eg composition, interfaces

---
+++ shout.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+type martian struct{}
+
+func (m martian) talk() string {
+	return "nack nack"
+}
+
+type laser int
+
+func (l laser) talk() string {
+	return strings.Repeat("pew", int(l))
+}
+
+type talker interface {
+	talk() string
+}
+
+func shout(t talker) {
+	louder := strings.ToUpper(t.talk())
+	fmt.Println(louder)
+}
+
+func main() {
+	shout(martian{})
+	shout(laser(2))
+}
